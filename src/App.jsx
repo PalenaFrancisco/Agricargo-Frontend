99b6b529b39ffd905920e870219bcb40f8ee6477
Francisco Palena <palenafrancisco@gmail.com>
2024-10-09 00:32:50 -0300

    data provider

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import ClientHome from "./pages/client/ClientHome";
 import ClientResult from "./pages/client/ClientResult";
 import Login from "./pages/Login Register/Login";
 import Register from "./pages/Login Register/Register";
+import { DataProvider } from "./components/context/DataProvider";
 
 function App() {
   return (
@@ -23,11 +24,13 @@ function App() {
       <Router>
         {/* <ClientHome /> */}
         <Routes>
-          <Route path="/" element={<ClientHome />}></Route>
           <Route
             path="/cliente/mis-reservas"
             element={<ClientReservations />}
           ></Route>
+          <Route path="/" element={
+            <ClientHome />
+            }></Route>
           <Route
             path="/cliente/mis-favoritos"
             // element={<ClientResult />}
--- src/components/searchForm/SearchForm.jsx
+++ src/components/searchForm/SearchForm.jsx
@@ -1,24 +1,35 @@
 import Input from "../input/Input"
 import Button from "../button/Button"
 import { IoSearch } from "react-icons/io5";
-import { useState } from "react";
+import { useDataContext } from "../context/DataProvider";
+import { fetchData } from "../../utils/fetchData";
 
 const SearchForm = ({ isSearchMode = true }) => {
-    const [origen, setOrigen] = useState("");
-    const [destino, setDestino] = useState("");
-    const [cantidad, setCantidad] = useState(0);
+    
+    const {inputValues, setInputValues, setResults, results} = useDataContext();
 
     const buttonText = isSearchMode ? "Buscar" : <IoSearch className="text-white text-2xl mr-0.5 mt-0.5"/>;
     const buttonClasses = isSearchMode
         ? "px-5 py-2.5 rounded-lg"
         : "rounded-full size-12 flex justify-center item-center";
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetchData("/trips.json")
+        .then(result =>{ 
+            setResults(result)
+            console.log(results)
+        })
+        .catch(error => console.log(error));
+    };
+
     return (
         <form className="flex items-center gap-4">
-            <Input setInputValue={setOrigen}>Origen</Input>
-            <Input setInputValue={setDestino}>Destino</Input>
-            <Input type="number" setInputValue={setCantidad}>Cantidad</Input>
-            <Button className={buttonClasses} actionClick={() => console.log((`${destino} => ${origen} => ${cantidad}`))}>{buttonText}</Button>
+            <Input setInputValue={(value) => setInputValues({...inputValues, origin: value})}>Origen</Input>
+            <Input setInputValue={(value) => setInputValues({...inputValues, destination: value})}>Destino</Input>
+            <Input type="number" setInputValue={(value) => setInputValues({...inputValues, quantity: value})}>Cantidad</Input>
+            <Button className={buttonClasses} actionClick={handleSubmit}>{buttonText}</Button>
         </form>
     );
 };

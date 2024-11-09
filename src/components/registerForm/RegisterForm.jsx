d138d1c7119d5892fb87d007ca5481563a1e800e
PalenaFrancisco <palenafrancisco@gmail.com>
2024-11-09 02:08:35 -0300

    validation login and register

--- src/components/registerForm/RegisterForm.jsx
+++ src/components/registerForm/RegisterForm.jsx
@@ -2,7 +2,7 @@ import { FaUser, FaEnvelope, FaLock } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import Button from "../button/Button";
 import Input from "../input/Input";
-import { useAuthContext } from "../context/AuthProvider";
+// import { useAuthContext } from "../context/AuthProvider";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -10,25 +10,37 @@ const RegisterForm = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState("");  // Estado para manejar errores
+    const [errors, setErrors] = useState({});  // Estado para manejar errores
+    const [registerError, setRegisterError] = useState("")
     const role = "Client"
+    const [isLoading, setIsLoading] = useState(false);
 
     const navigate = useNavigate();  // Definir navigate
     const cleanInputs = () => {
         setName("");
         setEmail("");
         setPassword("");
-        setError("");  // Limpiar error al reiniciar los campos
+        setErrors({});  // Limpiar error al reiniciar los campos
+        setRegisterError("");
+    };
+
+    const validateFields = () => {
+        const newErrors = {};
+
+        if (!name) newErrors.name = "Nombre es requerido"
+        if (!email) newErrors.email = "Email es requerido"
+        if (!password) newErrors.password = "Contraseña es requerida"
+
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        // Validación simple para asegurarse de que los campos no estén vacíos
-        if (!name || !email || !password) {
-            setError("Por favor, llena todos los campos.");
-            return;
-        }
+        if (!validateFields()) return;
+
+        setIsLoading(true)
 
         try {
             const res = await fetch("https://localhost:7183/api/Auth/register", {
@@ -45,52 +57,65 @@ const RegisterForm = () => {
             });
 
             if (!res.ok) {
+                setRegisterError("Error en el registro. Inténtalo de nuevo.")
                 throw new Error("Error en el registro. Inténtalo de nuevo.");
             }
 
             // Limpiar los campos y redirigir a la página de login
             cleanInputs();
             navigate("/login");
-            console.log({
-                name,
-                email,
-                password,
-                role})
-
         } catch (error) {
-            setError(error.message || "Error al registrar. Inténtalo de nuevo.");
+            setRegisterError("Cuenta con email ya existente")
+            setIsLoading(false);
+            // setError(error.message || );
         }
     };
 
     return (
-        <div className="flex justify-center items-center pt-24">
+        <div className="flex justify-center items-center pt-10">
             <div className="bg-white p-8 rounded-lg shadow-md max-w-sm w-full">
                 <h2 className="text-2xl font-bold text-center text-gray-900">Registrarse</h2>
                 <p className="text-center text-gray-400 mb-6">Estamos para ayudarte</p>
 
-                {error && (
+                {registerError && (
                     <div className="text-red-500 text-center mb-4">
-                        {error}
+                        {registerError}
                     </div>
                 )}
 
                 <form className="space-y-4" onSubmit={handleSubmit}>
                     <div className="flex items-center gap-2">
                         <FaUser className="text-gray-400" />
-                        <Input inputclass={'w-full'} setInputValue={(value) => setName(value)}>Tu Nombre</Input>
+                        <Input inputclass={'w-full'} setInputValue={(value) => setName(value)}
+                        inputstyle={errors.name ? "border-red-500" : ""}>Tu Nombre</Input>
                     </div>
+                    {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
 
                     <div className="flex items-center gap-2">
                         <FaEnvelope className="text-gray-400" />
-                        <Input type="email" inputclass={'w-full'} setInputValue={(value) => setEmail(value)}>Tu email</Input>
+                        <Input type="email" inputclass={'w-full'} setInputValue={(value) => setEmail(value)}
+                        inputstyle={errors.email ? "border-red-500" : ""}>Tu email</Input>
                     </div>
+                    {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
 
                     <div className="flex items-center gap-2">
                         <FaLock className="text-gray-400" />
-                        <Input type="password" inputclass={'w-full'} setInputValue={(value) => setPassword(value)}>Contraseña</Input>
+                        <Input type="password" inputclass={'w-full'} setInputValue={(value) => setPassword(value)}
+                        inputstyle={errors.password ? "border-red-500" : ""}>Contraseña</Input>
                     </div>
+                    {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
 
-                    <Button className="w-full py-2 rounded-lg font-semibold" typeButton={"submit"}>Crear cuenta</Button>
+                    <Button
+                        className="w-full py-2 rounded-lg font-semibold "
+                        typeButton="submit"
+                        disabled={isLoading} // Desactivar el botón mientras cargando
+                    >
+                        {isLoading ? (
+                            <div className="w-6 h-6 ml-32 border-4 border-t-4 border-gray-200 rounded-full border-t-blue-600 animate-spin"></div>
+                        ) : (
+                            "Crear cuenta"
+                        )}
+                    </Button>
 
                     <div className="flex items-center justify-center my-4">
                         <span className="border-t flex-grow"></span>
--- src/layout/LoginLayout.jsx
+++ src/layout/LoginLayout.jsx
@@ -1,11 +1,16 @@
-
+import { RxCross2 } from "react-icons/rx";
 import Logo from '../assets/logo.svg'
+import { useNavigate } from "react-router-dom";
 
 const LoginLayout = ({ children }) => {
+  const navigate = useNavigate();
+
   return (
     <>
-        <nav className='w-full'>
+        <nav className='w-full flex justify-between'>
             <a href='/'><img src={Logo} alt="Logo de Agricargo" className="pt-8 pl-8" /></a>
+            <button onClick={() => navigate(-1)}
+              className="mr-5"><RxCross2 className="text-gray-500 text-3xl"/></button>
         </nav>
         <div>
             {children}
